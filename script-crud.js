f5e91f9f595c177de4ab700a3fa89d29c22ff931
Igor Rodrigues <igor.aguiar1@live.com>
2026-08-11 17:39:32 -0300

    feat: :sparkles: add: exibir ou esconder formulário ao clicar em 'Adicionar nova tarefa'

---
+++ script-crud.js
@@ -0,0 +1,8 @@
+// encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
