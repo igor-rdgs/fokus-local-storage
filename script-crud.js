689c283ff657e6b5f85903d0c2e9c905cc46827a
Igor Rodrigues <igor.aguiar1@live.com>
2026-08-14 10:14:00 -0300

    feat: :sparkles: add: seleciona a tarefa ao clicar

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,7 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelarFormulario = document.querySelector('.app__form-footer__button--cancel')
+const paragrafoDescricaoTarefaAtiva = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -49,6 +50,11 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefaAtiva.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
